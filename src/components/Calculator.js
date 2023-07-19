ac26a577677b0ade7ca959d2d76f133092dd1e15
jfranklin12 <j.franks725@gmail.com>
2023-07-19 16:48:53 -0700

    initial folder set up and calcualtor.js base creation

---
+++ src/components/Calculator.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Calculator = () => {
+
+    return (
+        <div className="calculator">
+            {/* display for calculator */}
+            <div id="display"></div>
+
+            {/* container for buttons */}
+            <div className="buttons">
+                {/* number buttons */}
+                {/* operator buttons */}
+                {/* clear button */}
+                {/* equal button */}
+                {/* decimal button */}
+
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default Calculator;
